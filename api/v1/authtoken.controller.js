e1a6a57193b8cf6d7c5e2afe79e05e9d97f2544e
Ethan-Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-08-19 16:29:42 +0800

    feat: use backend to get authtoken

---
+++ api/v1/authtoken.controller.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+
+export default class AuthTokenController {
+    static async getAuthToken(req, res, next) {
+        try {
+            const accessToken = req.body.accessToken
+
+            if (accessToken === undefined) {
+                throw new Error("Access Token required to get Authorisation Token")
+            }
+
+            const getToken = async (accessToken) => {
+                var apiOptions = {
+                    method: "POST",
+                    url: "https://projmatch.us.auth0.com/oauth/token",
+                    headers: {
+                        "content-type": "application/x-www-form-urlencoded",
+                    },
+                    data: new URLSearchParams({
+                        grant_type: "authorization_code",
+                        client_id: process.env.AUTH0_CLIENT_ID,
+                        client_secret: process.env.OAUTH_CLIENT_SECRET,
+                        audience: process.env.AUTH0_AUDIENCE,
+                        code: accessToken,
+                        redirect_uri: `${process.env.AUTH0_BASE_URL}/Load`,
+                    }),
+                };
+
+                axios
+                    .request(apiOptions)
+                    .then(function (res) {
+                        const responseBody = res.data;
+                        
+                        return responseBody["access_token"]
+                    })
+                    .catch(function (err) {
+                        console.error("Failed to get API Authentication Token with: ", err);
+                    });
+            }
+
+            const token = await getToken(accessToken)
+
+            if (token === undefined || token === "") {
+                throw new Error("Authorisation Token is empty/undefined")
+            }
+            
+            res.json({ status: "success", token: token})
+        } catch (err) {
+            res.status(500).json({ error: err.message })
+        }
+    }
+}
