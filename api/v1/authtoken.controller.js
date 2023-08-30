0f4899754ad00ad8ff6872f76743010ecf17d0f6
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-08-30 16:34:44 +0800

    feat: fixed Auth Token API

--- api/v1/authtoken.controller.js
+++ api/v1/authtoken.controller.js
@@ -9,34 +9,30 @@ export default class AuthTokenController {
                 throw new Error("Access Token required to get Authorisation Token")
             }
 
-            const getToken = async (accessToken) => {
-                var apiOptions = {
-                    method: "POST",
-                    url: "https://projmatch.us.auth0.com/oauth/token",
-                    headers: {
-                        "content-type": "application/x-www-form-urlencoded",
-                    },
-                    data: new URLSearchParams({
-                        grant_type: "authorization_code",
-                        client_id: process.env.AUTH0_CLIENT_ID,
-                        client_secret: process.env.AUTH0_CLIENT_SECRET,
-                        audience: process.env.AUTH0_AUDIENCE,
-                        code: accessToken,
-                        redirect_uri: `${process.env.AUTH0_BASE_URL}/Load`,
-                    }),
-                };
+            var apiOptions = {
+                method: "POST",
+                url: "https://projmatch.us.auth0.com/oauth/token",
+                headers: {
+                    "content-type": "application/x-www-form-urlencoded",
+                },
+                data: new URLSearchParams({
+                    grant_type: "authorization_code",
+                    client_id: process.env.AUTH0_CLIENT_ID,
+                    client_secret: process.env.AUTH0_CLIENT_SECRET,
+                    audience: process.env.AUTH0_AUDIENCE,
+                    code: accessToken,
+                    redirect_uri: `${process.env.AUTH0_BASE_URL}/Load`,
+                }),
+            };
 
-                try {
-                    const response = await axios.request(apiOptions)
-
-                    const accessToken = response.data["access_token"]
-                    return accessToken
-                } catch (err) {
-                    console.error("Failed to get API Authentication Token with: ", err);
-                }
-            }
-
-            const token = await getToken(accessToken)
+            let token
+            await axios.request(apiOptions).then((res) => {
+                token = res.data["access_token"]
+            })
+            .catch((err) => {
+                throw new Error(`Error in calling Auth0 OAuth: ${err}`)
+            })
+            
             if (token === undefined || token === "") {
                 throw new Error("Authorisation Token is empty/undefined")
             }
