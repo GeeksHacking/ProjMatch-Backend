405e1366725ba62436a2640177f5f79a27a589d7
Ethan Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-08-30 15:46:56 +0800

    feat: update Auth Token API

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.json())
 app.use("/api/v1/users", jwtCheck, users)
 app.use("/api/v1/images", jwtCheck, images)
 app.use("/api/v1/posts", jwtCheck, posts)
-app.use("/api/v1/authtoken", jwtCheck, authToken)
+app.use("/api/v1/authtoken", authToken)
 app.use("*", (req, res) => res.status(404).json({error: "Not Found"}))
 app.use((err, req, res, next) => {
     const status = err.status || 500
