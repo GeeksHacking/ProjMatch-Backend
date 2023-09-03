c93648c324c555e730ac03a7914542e8305331e7
Ethan-Chew <chew_ming_hong_ethan@s2019.ssts.edu.sg>
2023-09-03 14:10:00 +0800

    feat: start on v2 users API

--- dao/v2/UsersDAO.js
+++ dao/v2/UsersDAO.js
@@ -1,4 +1,5 @@
 import mongodb from "mongodb"
+import axios from "axios"
 
 let ObjectID
 if (process.env.NODE_ENV === 'test') {
@@ -21,11 +22,60 @@ export default class UsersDAOV2 {
         }
     }
 
-    static async getUser() {
+    static async getUser({
+        filters = null,
+        page = 0, 
+        usersPerPage = 100
+    } = {}) {
+        try {
+            // Query and Filter according to Filter Parameter
+            let query
 
+            if (filters) {
+                if ("username" in filters) {
+                    query = { "username": { $eq: filters["username"] } }
+                }
+
+                if ("email" in filters) {
+                    query = { "contact": { $eq: filters["email"] } }
+                }
+
+                if ("userID" in filters) {
+                    query = { "_id": new ObjectID(filters["userID"]) }
+                }
+            }
+
+            let cursor
+            try {
+                cursor = await users.find(query)
+            } catch (err) {
+                throw {
+                    "msg": "Unable to find users with specified query",
+                    "statusCode": 404
+                }
+            }
+
+            const displayCursor = cursor.limit(usersPerPage).skip(usersPerPage * page)
+            try {
+                const usersList = await displayCursor.toArray()
+                const totalUsers = await users.countDocuments(userQuery)
+
+                return { usersList, totalUsers }
+            } catch (err) {
+                throw {
+                    "msg": `Unable to convert cursor to array, or a problem occured when counting documents: ${err}`,
+                    "statusCode": 500
+                }
+            }
+        } catch (err) {
+            throw {
+                "msg": "Unnknown Error Occured",
+                "statusCode": 500
+            }
+        }
     }
 
-    static async postUser() {
+    static async postUser(username, contact, about, algoData, skills) {
 
     }
 
@@ -33,7 +83,16 @@ export default class UsersDAOV2 {
 
     }
 
-    static async putUser() {
+    static async putUser(id, update) {
+        try {
+            const response = await users.updateOne({ "_id": new ObjectID(id)}, { $set: update })
 
+            return response
+        } catch (err) {
+            throw {
+                "msg": err.message,
+                "statusCode": 500
+            }
+        }
     }
 }
